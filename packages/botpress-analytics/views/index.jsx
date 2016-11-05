a22561152fe4211750b9ae328fcf9a0a262a1ac7
Dany Fortin-Simard <dany.fortin-simard@hotmail.com>
2016-11-05 16:00:09 -0400

    First test with recharts react

--- packages/botpress-analytics/views/index.jsx
+++ packages/botpress-analytics/views/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Panel } from 'react-bootstrap'
 import axios from 'axios'
+const Recharts = require('recharts')
 
 import style from './style.scss'
 
@@ -28,9 +29,38 @@ export default class AnalyticsModule extends React.Component {
     </p>
   }
 
+  renderSimpleLineChart(){
+    console.log(Recharts)
+
+    const {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} = Recharts
+
+    const data = this.state.simpleLineChart
+
+    const SimpleLineChart = React.createClass({
+    	render () {
+      	return (
+        	<LineChart width={600} height={300} data={data}
+                margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+           <XAxis dataKey="name"/>
+           <YAxis/>
+           <CartesianGrid strokeDasharray="3 3"/>
+           <Tooltip/>
+           <Legend />
+           <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+           <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+        );
+      }
+    })
+
+    return <SimpleLineChart />
+  }
+
   renderGraphs(){
     return(
-      <div>{this.state.data}</div>
+      <div>
+        <div>{this.renderSimpleLineChart()}</div>
+      </div>
     )
   }
 
