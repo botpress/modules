4e2bc780894be2946bbf56614e8a27447effa3cc
Dany Fortin-Simard <dany.fortin-simard@hotmail.com>
2016-11-06 10:17:33 -0500

    Read and write on files

--- packages/botpress-analytics/index.js
+++ packages/botpress-analytics/index.js
@@ -1,4 +1,18 @@
 const Analytics = require('./analytics')
+const fs = require('fs')
+const path = require('path')
+
+
+const loadDataFromFile = (file) => {
+  if(!fs.existsSync(file)){
+    console.log("Analytics file (" + file + ") doesn\'t exist.")
+  }
+  return JSON.parse(fs.readFileSync(file, "utf-8"))
+}
+
+const saveDataToFile = (data, file) => {
+  fs.writeFileSync(file, stringify(data))
+}
 
 module.exports = {
   ingoing: function(event, next) {
@@ -11,6 +25,11 @@ module.exports = {
 
   },
   ready: function(skin) {
+    const rawDatafile = path.join(skin.projectLocation, skin.botfile.dataDir, 'skin-analytics.raw.json')
+    const chartsDatafile = path.join(skin.projectLocation, skin.botfile.dataDir, 'skin-analytics.charts.json')
+
+    rawData = loadDataFromFile(rawDatafile);
+    chartsData = loadDataFromFile(chartsDatafile)
 
     analytics = new Analytics(skin);
 
@@ -18,5 +37,6 @@ module.exports = {
     .get("/graphs", (req, res, next) => {
       res.send(analytics.getGraphsData())
     })
+
   }
 }
