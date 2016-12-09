41781fe44522befaca3beea8457de25b783e9bbe
Dany Fortin-Simard <dany.fortin-simard@hotmail.com>
2016-12-09 12:13:06 -0500

    initial commit UI

--- packages/botpress-scheduler/src/views/previous/index.jsx
+++ packages/botpress-scheduler/src/views/previous/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import style from './style.scss'
 
-export default class SchedulerModule extends React.Component {
+export default class Previous extends React.Component {
 
   render() {
     return <h4>This module is empty...</h4>
