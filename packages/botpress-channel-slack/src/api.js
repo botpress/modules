98bcdec9c49cf1326b8e26e71a6a1318b3c41159
Ray Shih <mnf.shih@gmail.com>
2016-12-20 23:51:26 +0800

    add send message api endpoint for manually testing

---
+++ packages/botpress-channel-slack/src/api.js
@@ -0,0 +1,7 @@
+// TODO handle channel = null
+export default (rtm, getChannel, router) => {
+  router.post('/sendMessage', (req, res) => {
+    rtm.sendMessage(req.body.message, getChannel().id)
+    res.status(200).end()
+  })
+}
