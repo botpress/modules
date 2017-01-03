5671a8ca501bc30ae7b8a92a94dc190e35ceb4fd
Ray Shih <mnf.shih@gmail.com>
2017-01-03 10:01:11 +0800

    refactor: adapter.js, index.js

--- packages/botpress-channel-slack/src/api.js
+++ packages/botpress-channel-slack/src/api.js
@@ -1,16 +1,13 @@
-// TODO handle channel == null
-export default (bp, getChannel, router) => {
+export default (router, {
+  sendText,
+  getStatus
+}) => {
   router.post('/sendMessage', (req, res) => {
-    const event =  {
-      type: 'text',
-      platform: 'slack',
-      text: req.body.message,
-      raw: {
-        channelId: getChannel().id
-      }
-    }
-
-    bp.middlewares.sendOutgoing(event)
+    sendText(req.body.message)
     res.status(200).end()
+  })
+
+  router.get('/status', (req, res) => {
+    res.json(getStatus())
   })
 }
--- packages/botpress-channel-slack/src/index.js
+++ packages/botpress-channel-slack/src/index.js
@@ -1,37 +1,70 @@
 import setupApi from './api'
 import createSendFuncs from './sendFuncs'
 import createConfig from './config'
-import SlackConnector from './slackConnector'
-import Middlewares from './middlewares'
+import createAdapter from './adapter'
 
-let slackConn
+// TODO refactor rename it for better naming consistency
+import SlackConnector from './slackConnector'
+
+// TODO remove this
+import { createTestIncomingMiddleware } from './testHelper'
+
+let adapter = null
+let slackConn = null
 
 const channelName = 'slack-module-test'
 let channel = null
 const getChannel = () => channel
 
+// TODO
+// 2. aggregate SlackConnector creation
+// 3. configurable slack api token
+//    - status management
+//      - no token
+//      - connection failed
+//    - update config api -> restart slack rtm if token changed
+
 module.exports = {
   init(bp) {
-    const config = createConfig(bp)
-
     bp.slack = createSendFuncs(bp.middlewares.sendOutgoing)
+    adapter = createAdapter(bp.middlewares)
 
+    // TODO this is test only
+    bp.middlewares.register(
+      createTestIncomingMiddleware(getChannel)
+    )
+  },
+
+  ready(bp) {
+    const router = bp.getRouter('botpress-slack')
+
+    // TODO handle channel == null
+    const sendText = message => {
+      bp.slack.sendText(message, getChannel().id)
+    }
+
+    const getStatus = () => ({
+      hasSlackApiToken: false,
+      isSlackConnected: false
+    })
+
+    setupApi(router, {
+      sendText,
+      getStatus
+    })
+
+    const config = createConfig(bp)
     const slackApiToken = config.slackApiToken.get()
     // TODO check token
-    slackConn = SlackConnector(slackApiToken, bp.middlewares.sendIncoming)
+    slackConn = SlackConnector(slackApiToken, adapter.sendIncoming)
 
     // TODO channel list api
     // TODO select channel api
     slackConn.authenticateP.done(data => {
       channel = data.channels.filter(c => c.name === channelName)[0]
+      adapter.setSlackConn(slackConn)
     })
 
-    Middlewares(slackConn, getChannel).forEach(bp.middlewares.register)
-  },
-
-  ready(bp) {
-    const router = bp.getRouter('botpress-slack')
-    setupApi(bp, getChannel, router)
     slackConn.connect()
   }
 }
