f60be09a72f50534a208e0c8d7b432fabd26287d
Ray Shih <mnf.shih@gmail.com>
2017-01-03 10:01:11 +0800

    refactor: adapter.js, index.js

---
+++ packages/botpress-channel-slack/src/adapter.js
@@ -0,0 +1,40 @@
+/**
+ * slack connector <-> adapter <-> middlware (sendIncoming, outgoingMiddleware)
+ */
+export default ({register, sendIncoming}) => {
+
+  let slackConn = null
+
+  const outgoingMiddleware = (event, next) => {
+    if (event.platform !== 'slack') {
+      return next()
+    }
+
+    // if (!outgoing[event.type]) {
+    //   return next('Unsupported event type: ' + event.type)
+    // }
+    //
+    // outgoing[event.type](event, next, messenger)
+
+    // TODO update slackConn connection state
+    if (!slackConn) return
+
+    const {text, raw: {channelId}} = event
+    slackConn.rtm.sendMessage(text, channelId)
+  }
+
+  register({
+    name: 'slack.sendMessages',
+    type: 'outgoing',
+    order: 100,
+    handler: outgoingMiddleware,
+    module: 'botpress-slack',
+    description: 'Sends out messages that targets platform = slack.' +
+    ' This middleware should be placed at the end as it swallows events once sent.'
+  })
+
+  return {
+    setSlackConn: conn => slackConn = conn,
+    sendIncoming,
+  }
+}
