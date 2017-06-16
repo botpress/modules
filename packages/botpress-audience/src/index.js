25f855f69fbd00fefc460c35fc9e7d0b2489b49e
Dany Fortin-Simard <dany@botpress.io>
2017-06-16 10:50:54 -0400

    Initial commit

---
+++ packages/botpress-audience/src/index.js
@@ -0,0 +1,31 @@
+/* global __dirname */
+
+import checkVersion from 'botpress-version-manager'
+
+module.exports = {
+
+  init: function(bp) {
+    checkVersion(bp, __dirname)
+  },
+
+  ready: function(bp) {
+    const router = bp.getRouter('botpress-audience')
+
+    router.post('/users', (req, res)=> {
+      const { from, limit } = req.body
+      bp.users.list(limit, from)
+      .then((values) => {
+        res.send(values)
+      })
+      .catch((err) => res.status.send(500).send({ message:err.message }))
+    })
+
+    router.get('/users/count', (req, res) => {
+      bp.users.count()
+      .then((count) => {
+        res.send(count)
+      })
+      .catch((err) => res.status.send(500).send({ message:err.message }))
+    })
+  }
+}
